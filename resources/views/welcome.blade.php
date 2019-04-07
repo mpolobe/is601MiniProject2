c028eae457db26935ce47d7230164df4f6095b83
Ben Mpolokoso <ben.mpolokoso@gmail.com>
2019-04-07 02:35:41 +0200

    Task:  Create a controller, route, and view to show an about page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,6 +92,8 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/about">About</a>
                 </div>
             </div>
         </div>
