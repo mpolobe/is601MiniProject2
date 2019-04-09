0eb9b97f2c89e71a0f152056fca42dead27e3342
Ben Mpolokoso <ben.mpolokoso@gmail.com>
2019-04-09 22:25:18 +0200

    Task: A unit test to check if a car's Model is a string

---
+++ tests/Unit/ModelTypeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Car;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ModelTypeTest extends TestCase
+{
+    public function testCarString()
+    {
+        $car = Car::all();
+        $car = Car::find(12);
+        $this->assertIsString('string', $car->model);
+    }
+}
