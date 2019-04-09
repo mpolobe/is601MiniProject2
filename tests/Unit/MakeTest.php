3c3cc947eb02220de3ededfc315abf7936fbd439
Ben Mpolokoso <ben.mpolokoso@gmail.com>
2019-04-09 22:17:45 +0200

    Task: A unit test to check if a car's Make is Ford, Honda or Toyota

---
+++ tests/Unit/MakeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Car;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MakeTest extends TestCase
+{
+    public function testCarModel()
+    {
+      //  $cars = Car::all();
+        $car = Car::inRandomOrder()->first();
+        if ($car->make = "Ford") {
+            $this->assertTrue(true);
+        }
+        elseif ($car->make = "Honda") {
+            $this->assertTrue(true);
+        }
+        else ($car->make = "Toyota");
+        {
+            $this->assertTrue(true);
+        }
+    }
+}
