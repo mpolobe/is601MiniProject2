4d540d1d6b1cc200d617ab311ca0573d522b27f1
Ben Mpolokoso <ben.mpolokoso@gmail.com>
2019-04-09 21:49:50 +0200

    Task: A unit test to count the number of records inserted by the database seed

---
+++ tests/Unit/YearIntTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Car;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class YearIntTest extends TestCase
+{
+    public function testCarYear()
+    {
+        $car = Car::find(30);
+        $this->assertInternalType('numeric',$car->year);
+    }
+}
